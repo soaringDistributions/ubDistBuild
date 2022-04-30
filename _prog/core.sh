c8f9843d7ea11a28415738f78dd3fc81d3a53a1f
mirage335 <spamfreemirage335@gmail.com>
2022-04-30 01:41:31 -0400

    Draft.

--- _prog/core.sh
+++ _prog/core.sh
@@ -1138,15 +1138,15 @@ _create_kde() {
 	cd "$HOME"
 	
 	
-	cp -r "$scriptLib"/custom/license_package_kde "$HOME"/
+	cp -r "$scriptLib"/custom/license_package_kde "$HOME"/.license_package_kde
 	
 	rm -f "$scriptLocal"/package_kde.tar.xz > /dev/null 2>&1
 	#-T0
-	env XZ_OPT="-e9" tar --exclude='./.config/chromium' -cJvf "$scriptLocal"/package_kde.tar.xz ./.config ./.kde ./.local ./license_package_kde
+	env XZ_OPT="-e9" tar --exclude='./.config/chromium' -cJvf "$scriptLocal"/package_kde.tar.xz ./.config ./.kde ./.local ./.license_package_kde
 	
-	rm -f "$HOME"/license_package_kde/license.txt
-	rm -f "$HOME"/license_package_kde/CC0_license.txt
-	rmdir "$HOME"/license_package_kde
+	rm -f "$HOME"/.license_package_kde/license.txt
+	rm -f "$HOME"/.license_package_kde/CC0_license.txt
+	rmdir "$HOME"/.license_package_kde
 }
 
 
