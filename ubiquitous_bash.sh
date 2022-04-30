8adeb8b464c1c363386711c727acd780a004d41e
mirage335 <spamfreemirage335@gmail.com>
2022-04-29 22:54:24 -0400

    Correction.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='3296277855'
+export ub_setScriptChecksum_contents='1799045280'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -37994,7 +37994,7 @@ _chroot_test() {
 _create_kde() {
 	rm -f "$scriptLocal"/package_kde.tar.xz > /dev/null 2>&1
 	#-T0
-	env XZ_OPT="-e9" tar --exclude='./folder' --exclude='./.config/chromium' -cJvf "$scriptLocal"/package_kde.tar.xz "$HOME".config "$HOME".kde "$HOME".local
+	env XZ_OPT="-e9" tar --exclude='./folder' --exclude='./.config/chromium' -cJvf "$scriptLocal"/package_kde.tar.xz "$HOME"./config "$HOME"/.kde "$HOME"/.local
 }
 
 
