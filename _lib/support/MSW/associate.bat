3c4265591873da4fe4d78ebdc46610c022cf1c1b
mirage335 <spamfreemirage335@gmail.com>
2023-08-17 23:52:58 -0400

    Untracked files.

---
+++ _lib/support/MSW/associate.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM https://www.reddit.com/r/bashonubuntuonwindows/comments/9ax19o/how_to_set_wsl_program_as_default_program_for/
+
+REM ftype geda.schematic=wslg.exe ~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _wrap gschem $(wslpath "%1")
+
+
+
+echo BATCH: Setting file type association...
+
+assoc %2=geda.schematic
+
+:: Use double percentage to escape %1
+ftype %1=wslg.exe ~/.ubcore/ubiquitous_bash/ubiquitous_bash.sh _wrap %3 $(wslpath "%%1")
+
+echo BATCH: Done.
+exit /b
+
+
+
+
