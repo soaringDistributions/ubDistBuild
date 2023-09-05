dac7d6037b67ffece5b41b17ec267816f54aeb64
mirage335 <spamfreemirage335@gmail.com>
2023-09-04 20:59:09 -0400

    Draft.

---
+++ _prog/convert-live-exhaustive.sh
@@ -0,0 +1,54 @@
+
+_live_sequence_exhaustive() {
+    _start
+    
+    _messagePlain_nominal 'copy: vm.img'
+    mkdir -p "$safeTmp"/NOTmounted
+    if ! cp "$scriptLocal"/vm.img "$safeTmp"/NOTmounted/vm.img
+    then
+        _messageFAIL
+        _stop 1
+    fi
+    
+    sudo -n mksquashfs "$safeTmp"/NOTmounted "$scriptLocal"/livefs/image/live/filesystem.squashfs -b 262144 -no-xattrs -noI -noX -comp lzo -Xalgorithm lzo1x_1
+	du -sh "$scriptLocal"/livefs/image/live/filesystem.squashfs
+
+    _stop
+}
+
+_convert-live-exhaustive() {
+	_messageNormal '_convert: vm-live-exhaustive.iso'
+
+    if [[ ! -e "$scriptLocal"/vm-live.iso ]]
+    then
+        _messagePlain_bad 'bad: missing: vm-live.iso'
+        _messagePlain_request 'request: _convert-live (adds desirable changes for slow-throughput slow-seek and improves revert capability)'
+        _messageFAIL
+        _stop 1
+        return 1
+	fi
+
+    if ! "$scriptAbsoluteLocation" _live_sequence_exhaustive "$@"
+    then
+        _messageFAIL
+        _stop 1
+    fi
+	
+	if ! "$scriptAbsoluteLocation" _live_sequence_in "$@"
+	then
+		_stop 1
+	fi
+	
+	[[ "$current_diskConstrained" == "true" ]] && rm -f "$scriptLocal"/vm.img
+	
+	if ! "$scriptAbsoluteLocation" _live_sequence_out "$@"
+	then
+		_stop 1
+	fi
+	
+	_safeRMR "$scriptLocal"/livefs
+}
+
+
+
+
