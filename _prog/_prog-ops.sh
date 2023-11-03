5327e7afa43b252cc98640258b4f3f7f431722ca
mirage335 <spamfreemirage335@example.com>
2023-11-03 17:02:15 -0400

    Draft.

--- _prog/_prog-ops.sh
+++ _prog/_prog-ops.sh
@@ -2,3 +2,13 @@
 [[ -e "$scriptAbsoluteFolder"/_prog-ops/revert-live.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/revert-live.sh
 
 [[ -e "$scriptAbsoluteFolder"/_prog-ops/convert-live-exhaustive.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/convert-live-exhaustive.sh
+
+
+
+[[ -e "$scriptAbsoluteFolder"/_prog-ops/get-custom.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/get-custom.sh
+
+
+
+
+
+
