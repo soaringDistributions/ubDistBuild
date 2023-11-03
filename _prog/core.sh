d3ff52a05284c98c643eeb71532f17ee4182cf82
mirage335 <spamfreemirage335@example.com>
2023-11-03 12:29:10 -0400

    Draft.

--- _prog/core.sh
+++ _prog/core.sh
@@ -2358,7 +2358,7 @@ _create_kde() {
 	
 	rm -f "$scriptLocal"/package_kde.tar.xz > /dev/null 2>&1
 	#-T0
-	env XZ_OPT="-e9" tar --exclude= './.ubtmp' --exclude='./.config/chromium' --exclude './.config/systemd/user/bootdiscStartup.service' --exclude='./.config/startup.sh' --exclude='./.config/autostart/startup.desktop' --exclude='./.config/plasma-workspace/env/startup.sh' --exclude'./.config/qt5ct' -cJvf "$scriptLocal"/package_kde.tar.xz ./.config ./.kde ./.local ./.xournal/config ./.license_package_kde
+	env XZ_OPT="-e9" tar --exclude='./.ubtmp' --exclude='./.config/chromium' --exclude='./.config/systemd/user/bootdiscStartup.service' --exclude='./.config/startup.sh' --exclude='./.config/autostart/startup.desktop' --exclude='./.config/plasma-workspace/env/startup.sh' --exclude='./.config/qt5ct' --exclude='./.local/state' --exclude='./.local/share/virtualenv' --exclude='./.config/VirtualBox' --exclude='./.config/gcloud' --exclude='./.config/qalculate' --exclude='./.config/pulse' --exclude='./.config/systemd' --exclude='./.local/share/RecentDocuments' --exclude='./.local/share/recently-used.xbel' --exclude='./.local/share/kwalletd' --exclude='./.local/share/keyrings' -cJvf "$scriptLocal"/package_kde.tar.xz ./.config ./.kde ./.local ./.xournal/config ./.license_package_kde
 	
 	rm -f "$HOME"/.license_package_kde/license.txt
 	rm -f "$HOME"/.license_package_kde/CC0_license.txt
