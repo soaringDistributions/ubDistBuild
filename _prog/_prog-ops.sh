d3086275393cbcfedc0cb0c7564f93a4f1f4fde7
mirage335 <spamfreemirage335@example.com>
2023-11-05 10:32:44 -0500

    Documentation.

--- _prog/_prog-ops.sh
+++ _prog/_prog-ops.sh
@@ -1,4 +1,7 @@
 
+[[ -e "$scriptAbsoluteFolder"/_prog-ops/ops-custom.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/ops-custom.sh
+
+
 [[ -e "$scriptAbsoluteFolder"/_prog-ops/revert-live.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/revert-live.sh
 
 [[ -e "$scriptAbsoluteFolder"/_prog-ops/convert-live-exhaustive.sh ]] && . "$scriptAbsoluteFolder"/_prog-ops/convert-live-exhaustive.sh
