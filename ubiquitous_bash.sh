04d467cc28bd9401abdf79017b077d38c5b42abc
mirage335 <spamfreemirage335@gmail.com>
2023-12-02 23:59:21 -0500

    Compile.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='2770714433'
+export ub_setScriptChecksum_contents='2083553593'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -45107,7 +45107,8 @@ _create_ubDistBuild-install-ubDistBuild() {
 	sudo -n rsync -ax "$scriptAbsoluteFolder"/_local/.gitignore "$globalVirtFS"/home/user/ubDistBuild/_local/
 	sudo -n rsync -ax "$scriptAbsoluteFolder"/_local/ubcp "$globalVirtFS"/home/user/ubDistBuild/_local/
 	
-	sudo -n rsync -ax --exclude "vm.img" --exclude "vm-live.iso" --exclude "package_rootfs.tar" --exclude "vm.img.*" --exclude "vm-live.iso.*" --exclude "package_rootfs.tar.*" "$scriptAbsoluteFolder"/_local/. "$globalVirtFS"/home/user/ubDistBuild/_local/
+	#--exclude "gsysd.log"
+	sudo -n rsync -ax --exclude "vm.img" --exclude "vm-live.iso" --exclude "package_rootfs.tar" --exclude "vm.img.*" --exclude "vm-live.iso.*" --exclude "package_rootfs.tar.*" --exclude "WARNING" --exclude "l_o" --exclude "l_o-chrt" --exclude "imagedev" "$scriptAbsoluteFolder"/_local/. "$globalVirtFS"/home/user/ubDistBuild/_local/
 	
 	_chroot chown -R user:user /home/user/ubDistBuild
 	_chroot chmod 700 /home/user/ubDistBuild
@@ -49182,6 +49183,11 @@ _revert-fromLive() {
 	
 	_set_ubDistBuild
 	
+	
+	#rm -f "$scriptLocal"/l_o
+	#rm -f "$scriptLocal"/l_o-chrt
+	#rm -f "$scriptLocal"/imagedev
+	#rm -f "$scriptLocal"/WARNING
 	
 	
 	_createVMimage
