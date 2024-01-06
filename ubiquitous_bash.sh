9a98e44ff05067bb18e7ab90e3b4af48b9090890
mirage335 <spamfreemirage335@gmail.com>
2024-01-06 18:55:29 -0500

    Draft.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='2230711505'
+export ub_setScriptChecksum_contents='4035154080'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -44803,7 +44803,7 @@ _custom_kernel_server-sequence() {
 		sudo -n cp -f "$globalVirtFS"/home/user/core/installations/kernel_linux/linux-lts-server-amd64-debian.tar.gz "$globalVirtFS"/
 	fi
 	_chroot tar xf /linux-lts-server-amd64-debian.tar.gz
-	_chroot dpkg -i './lts-server/*.deb'
+	_chroot bash -c 'dpkg -i ./lts-server/*.deb'
 	_chroot rm -f ./lts-server/.config './lts-server/linux-*' ./lts-server/statement.sh.out.txt
 	_chroot rm -f ./lts-server/linux-lts-server-amd64-debian.tar.gz
 	_chroot rm -f /linux-lts-server-amd64-debian.tar.gz
