0f6fa58e0a7e5195dc5c84d62d34c3e7b9535931
mirage335 <spamfreemirage335@gmail.com>
2024-12-31 00:28:14 -0500

    Documentation.

--- _prog-ops/ops-custom.example.sh
+++ _prog-ops/ops-custom.example.sh
@@ -2,6 +2,54 @@ _custom() {
 	true
 }
 
+# ATTENTION: Actually SHRINKS disk image.
+# Overloaded function.
+# WARNING: May be untested.
+# WARNING: May require removing much software, especially GUI software, to shrink to ~15GB. Intended for embedded (ie. Klipper, etc).
+# ATTRIBUTION-AI: ChatGPT o1 2024-12-30 . (partially)
+_custom-expand() {
+	local currentExitStatus
+	currentExitStatus="0"
+	
+	_messageNormal '_custom-expand: SHRINK'
+
+	! _openChRoot && _messagePlain_bad 'fail: openChRoot' && _messageFAIL
+	
+	! _messagePlain_probe_cmd _chroot btrfs filesystem resize 15000000000 / && _messageFAIL
+	
+	! _closeChRoot && _messagePlain_bad 'fail: closeChRoot' && _messageFAIL
+
+
+
+	_messageNormal '_custom-expand: SHRINK: parted'
+	! _openLoop && _messagePlain_bad 'fail: openLoop' && _messageFAIL
+	
+	export ubVirtPlatform="x64-efi"
+	#_determine_rawFileRootPartition
+	
+	export ubVirtImagePartition="p5"
+
+	local current_imagedev=$(cat "$scriptLocal"/imagedev)
+	local current_rootpart=$(echo "$ubVirtImagePartition" | tr -dc '0-9')
+	
+	# GPT partition backup table, if relevant, at end of disk, will normally also be moved by the 'resizepart' parted command.
+	! _messagePlain_probe_cmd sudo -n parted --align optimal --script "$current_imagedev" resizepart "$current_rootpart" 15000000000B && _messageFAIL
+	
+	unset ubVirtPlatform
+	unset ubVirtImagePartition
+
+	! _closeLoop && _messagePlain_bad 'fail: closeLoop' && _messageFAIL
+
+
+
+	_messageNormal '_custom-expand: SHRINK: truncate'
+	# CAUTION: Must be larger than the end location of partition, due to possible GPT backup table, etc.
+	! _messagePlain_probe_cmd truncate --size=15003145728B "$scriptLocal"/vm.img && _messageFAIL
+
+
+
+	return 0
+}
 _custom-expand() {
 	local currentExitStatus
 	currentExitStatus="0"
